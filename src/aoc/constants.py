5230ba7f946d8795ed73379c31ed24e6e3df1438
enrico <enrico.dsf@gmail.com>
2025-01-03 21:47:50 +0100

    updated constant

--- src/aoc/constants.py
+++ src/aoc/constants.py
@@ -5,7 +5,9 @@ ROOT_DIR = Path(__file__).parent
 
 
 # solutions.py template
-SOLUTIONS_TEMPLATE = """
+SOLUTIONS_TEMPLATE = """from pathlib import Path
+
+
 def main(input_path: Path):
     pass    
 """
