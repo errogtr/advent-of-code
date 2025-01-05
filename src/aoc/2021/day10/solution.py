cf3a0aefba81be2719b2e43bbc191824dc39d4b8
enrico <enrico.dsf@gmail.com>
2025-01-05 02:37:05 +0100

    y2021 d10 p2 solution

--- src/aoc/2021/day10/solution.py
+++ src/aoc/2021/day10/solution.py
@@ -1,8 +1,12 @@
 from pathlib import Path
+from statistics import median
 
-
-CLOSURES = dict(zip("([{<", ")]}>"))
-ERROR_SCORES = dict(zip(")]}>", [3, 57, 1197, 25137]))
+LEFT = "([{<"
+RIGHT = ")]}>"
+CLOSURES = dict(zip(LEFT, RIGHT))
+OPENINGS = dict(zip(RIGHT, LEFT))
+ERROR_SCORES = dict(zip(RIGHT, [3, 57, 1197, 25137]))
+AUTOCOMPLETE_SCORES = dict(zip(LEFT, range(1, 5)))
 
 
 def main(input_data: Path):
@@ -10,6 +14,7 @@ def main(input_data: Path):
         lines = f.read().splitlines()
 
     error_score = 0
+    autocomplete_scores = list()
     for line in lines:
         stack = list()
         for c in line:
@@ -20,5 +25,14 @@ def main(input_data: Path):
             else:
                 error_score += ERROR_SCORES[c]
                 break
-    
+        else:
+            score = 0
+            while stack:
+                score = score * 5 + AUTOCOMPLETE_SCORES[stack.pop()]
+            autocomplete_scores.append(score)
+
+    # ==== PART 1 ====
     print(error_score)
+
+    # ==== PART 2 ====
+    print(median(autocomplete_scores))
