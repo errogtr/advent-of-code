e90248d8a6f08e512a4282b342daf2203b9294a1
enrico <enrico.dsf@gmail.com>
2025-01-08 00:53:07 +0100

    y2021 d16 refactor

--- src/aoc/2021/day16/solution.py
+++ src/aoc/2021/day16/solution.py
@@ -1,79 +1,92 @@
 from itertools import batched
 from math import prod
 from pathlib import Path
-
-
-OpsMap = dict[str, callable]
+from typing import Callable
 
 
 def hex2bin(c: str) -> str:
     return bin(int(c, 16))[2:]
 
 
-def parse(packet: str, ops: OpsMap) -> int:
+def parse(packet: str) -> tuple[int, int, int]:
     version, type_id = int(packet[:3], 2), packet[3:6]
     if type_id == "100":  # literal
-        value = ""
-        i = 6
-        for prefix, *group in batched(packet[i:], 5):
-            value += "".join(group)
-            i += 5
-            if prefix == "0":
-                break
-        return i, version, int(value, 2)
-    
-    values = list()
-    length_type_id = packet[6]
-    if length_type_id == "0":
-        i = 22
-        l = int(packet[7:i], 2)
-        while i - 22 < l:
-            offset, version_sub, value = parse(packet[i:], ops)
-            version += version_sub
-            i += offset
-            values.append(value)
+        value, i = literal(packet)
+        return version, value, i
+
+    if packet[6] == "0":
+        version, values, i = op_type_zero(packet, version)
     else:  # length_type_id == "1"
-        i = 18
-        n = int(packet[7:i], 2)
-        for _ in range(n):
-            offset, version_sub, value = parse(packet[i:], ops)
-            version += version_sub
-            i += offset
-            values.append(value)
-    return i, version, ops[type_id](values)
+        version, values, i = op_type_one(packet, version)
+
+    match type_id:
+        case "000": value = sum(values)
+        case "001": value = prod(values)
+        case "010": value = min(values)
+        case "011": value = max(values)
+        case "101": value = gt(*values)
+        case "110": value = lt(*values)
+        case "111": value = eq(*values)
+
+    return version, value, i
 
 
-def gt(values) -> bool:
-    x, y, *_ = values
+def literal(packet) -> tuple[int, int]:
+    value = ""
+    i = 6
+    for prefix, *group in batched(packet[i:], 5):
+        value += "".join(group)
+        i += 5
+        if prefix == "0":
+            break
+    return int(value, 2), i
+
+
+def op_type_zero(packet: str, version: int) -> tuple[int, list[int], int]:
+    i = 22
+    l = int(packet[7:i], 2)
+    values = list()
+    while i - 22 < l:
+        version_sub, value, offset = parse(packet[i:])
+        version += version_sub
+        i += offset
+        values.append(value)
+    return version, values, i
+
+
+def op_type_one(packet: str, version: int) -> tuple[int, list[int], int]:
+    values = list()
+    i = 18
+    n = int(packet[7:i], 2)
+    for _ in range(n):
+        version_sub, value, offset = parse(packet[i:])
+        version += version_sub
+        i += offset
+        values.append(value)
+    return version, values, i
+
+
+def gt(x: int, y: int) -> bool:
     return x > y
 
-def lt(values) -> bool:
-    x, y, *_ = values
+
+def lt(x: int, y: int) -> bool:
     return x < y
 
-def eq(values) -> bool:
-    x, y, *_ = values
+
+def eq(x: int, y: int) -> bool:
     return x == y
 
 
 def main(input_path: Path):
-    ops = {
-        "000": sum,
-        "001": prod,
-        "010": min,
-        "011": max,
-        "101": gt,
-        "110": lt,
-        "111": eq,
-    }
     with input_path.open() as f:
         hex_packet = f.read()
 
     packet = "".join(hex2bin(c).zfill(4) for c in hex_packet)
-    _, version, value = parse(packet, ops)
+    version, value, _ = parse(packet)
 
     # ==== PART 1 ====
-    print(version)    
+    print(version)
 
     # ==== PART 2 ====
-    print(value) 
+    print(value)
