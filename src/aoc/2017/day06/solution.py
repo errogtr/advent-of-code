86c6bbd59c39e40c8c2064d6cd09589db489e4df
enrico <enrico.dsf@gmail.com>
2025-07-09 23:19:19 +0200

    y2016d6p1 solution

--- src/aoc/2017/day06/solution.py
+++ src/aoc/2017/day06/solution.py
@@ -2,4 +2,24 @@ from pathlib import Path
 
 
 def main(input_path: Path):
-    pass    
+    with input_path.open() as f:
+        blocks = dict(enumerate(map(int, f.read().split())))
+    
+    seen = {"".join(map(str, blocks.values()))}
+    loop_start = None
+    cycles = 1
+    while True:
+        i, max_val = max(blocks.items(), key=lambda x: x[1])
+        blocks[i] = 0
+        while max_val:
+            i = (i + 1) % len(blocks)
+            blocks[i] += 1
+            max_val -= 1
+        state = "".join(map(str, blocks.values()))
+        if state in seen:
+            break
+        seen.add(state)
+        cycles += 1
+    
+    print(cycles)
+
