45c32813256434a34e8aef00655a95b2438d699d
enrico <enrico.dsf@gmail.com>
2025-07-17 02:27:41 +0200

    y2017d9 cleaner

--- src/aoc/2017/day09/solution.py
+++ src/aoc/2017/day09/solution.py
@@ -5,50 +5,35 @@ def main(input_path: Path):
     with input_path.open() as f:
         stream = f.read()
     
-    score = 0
+    scores = list()
     total_score = 0
     total_garbage = 0
-    garbage_starts = 0
     garbage = False
-    group_start = list()
-    i = 0
     while stream:
-        c = stream[i]
+        c, stream = stream[0], stream[1:]
 
         if c == "!":
-            stream = stream[:i] + stream[i+2:]
+            stream = stream[1:]
 
         elif garbage and c != ">":
-            i += 1
             total_garbage += 1
 
         elif c == "<":
-            garbage_starts = i
             garbage = True
-            i += 1
         
         elif c == ">":
             garbage = False
-            stream = stream[:garbage_starts] + stream[i+1:]
-            i = garbage_starts
 
-        elif c == "{" and not garbage:
-            score += 1
-            group_start.append(i)
-            i += 1
+        elif c == "{":
+            scores.append(scores[-1] + 1 if scores else 1)
 
-        elif c == "}" and not garbage:             
-            current_group_start = group_start.pop()
-            total_score += score
-            stream = stream[:current_group_start] + stream[i+1:]
-            i = current_group_start
-            score -= 1
-        
-        else:
-            stream = stream[:i] + stream[i+1:]
+        elif c == "}":             
+            total_score += scores.pop()
         
 
+    # ==== PART 1 ====
     print(total_score)
 
+    # ==== PART 2 ====
     print(total_garbage)
 
