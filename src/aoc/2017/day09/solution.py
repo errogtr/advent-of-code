0a130cbb35428ed6c71049ce356f0c8ee9aca79f
enrico <enrico.dsf@gmail.com>
2025-07-17 02:37:46 +0200

    minor change in y2017d9

--- src/aoc/2017/day09/solution.py
+++ src/aoc/2017/day09/solution.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 
-    
+
 def main(input_path: Path):
     with input_path.open() as f:
         stream = f.read()
-    
-    scores = list()
-    total_score = 0
+
+    scores_depth = list()
+    score = 0
     total_garbage = 0
     garbage = False
     while stream:
@@ -14,26 +14,20 @@ def main(input_path: Path):
 
         if c == "!":
             stream = stream[1:]
-
-        elif garbage and c != ">":
-            total_garbage += 1
-
+        elif garbage:
+            if c == ">":
+                garbage = False
+            else:
+                total_garbage += 1
         elif c == "<":
             garbage = True
-        
-        elif c == ">":
-            garbage = False
-
         elif c == "{":
-            scores.append(scores[-1] + 1 if scores else 1)
-
-        elif c == "}":             
-            total_score += scores.pop()
-        
+            scores_depth.append(scores_depth[-1] + 1 if scores_depth else 1)
+        elif c == "}":
+            score += scores_depth.pop()
 
     # ==== PART 1 ====
-    print(total_score)
+    print(score)
 
     # ==== PART 2 ====
     print(total_garbage)
-
