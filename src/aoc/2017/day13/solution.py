3324e9cb2130eee8d40a4daaa2b0f99d0de3aabe
enrico <enrico.dsf@gmail.com>
2025-07-21 17:54:50 +0200

    y2017d13 solution (sub-optimal)

--- src/aoc/2017/day13/solution.py
+++ src/aoc/2017/day13/solution.py
@@ -1,5 +1,27 @@
+from itertools import count
 from pathlib import Path
 
 
+def get_severity(firewall, delay):
+    severity = 0
+    for idx, depth in firewall.items():
+        if (idx + delay) % (2 * (depth - 1)) == 0:
+            severity += depth * (idx + delay)
+            if delay:
+                break
+    return severity
+
+
 def main(input_path: Path):
-    pass
+    with input_path.open() as f:
+        firewall = dict(map(int, line.split(": ")) for line in f.read().splitlines())
+
+    # ==== PART 1 ====
+    print(get_severity(firewall, delay=0))
+
+
+    # ==== PART 2 ====
+    for delay in count(1):
+        if get_severity(firewall, delay) == 0:
+            break
+    print(delay)
