28ac636ffec28b3318bfb5dfbfed82528da1afc5
enrico <enrico.dsf@gmail.com>
2025-07-22 04:27:25 +0200

    y2017d17 solution

--- src/aoc/2017/day17/solution.py
+++ src/aoc/2017/day17/solution.py
@@ -2,4 +2,19 @@ from pathlib import Path
 
 
 def main(input_path: Path):
-    pass
+    with input_path.open() as f:
+        steps = int(f.read())
+
+    buffer, curr, target = [0], 0, 0
+    for t in range(1, 50_000_000):
+        curr = (curr + steps) % t + 1
+        if t <= 2017:
+            buffer.insert(curr, t)
+        if curr == 1:
+            target = t
+
+    # ==== PART 1 ====
+    print(buffer[buffer.index(2017) + 1])
+
+    # ==== PART 2 ====
+    print(target)
