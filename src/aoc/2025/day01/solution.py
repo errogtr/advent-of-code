4217c3ddf1fd79b769e6bd2d4672c93bb5a54ddf
enrico <enrico.dsf@gmail.com>
2025-12-01 12:03:17 +0100

    AoC 2025 d1 - badly written solution

--- src/aoc/2025/day01/solution.py
+++ src/aoc/2025/day01/solution.py
@@ -1,5 +1,44 @@
 from pathlib import Path
 
 
+def parse(line: str) -> tuple[str, int]:
+    direction, amount = line[0], line[1:]
+    return direction, int(amount)
+
+
 def main(input_path: Path):
-    pass    
+    with input_path.open() as f:
+        rotations = [parse(line) for line in f.read().splitlines()]
+
+    # ==== PART 1 ====
+    pos = 50
+    zeroes = 0
+    for direction, amount in rotations:
+        sign = (-1) ** (direction == "L")
+        pos = (pos + sign * amount) % 100
+        if pos == 0:
+            zeroes += 1
+
+    print(zeroes)
+
+
+    # ==== PART 2 ====
+    pos = 50
+    zeroes = 0
+    for direction, amount in rotations:
+        sign = (-1) ** (direction == "L")
+        new_pos = pos + sign * amount
+        if new_pos >= 100:
+            zeroes += new_pos // 100
+        elif new_pos == 0:
+            zeroes += 1
+        elif new_pos < 0:
+            zeroes += - (new_pos // 100)
+            if pos == 0:
+                zeroes -= 1
+            if new_pos % 100 == 0:
+                zeroes += 1
+
+        pos = new_pos % 100
+        
+    print(zeroes)
