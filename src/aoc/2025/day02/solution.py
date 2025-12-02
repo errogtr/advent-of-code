d88be0558e93129e8db1088ad5c694ddee986063
enrico <enrico.dsf@gmail.com>
2025-12-02 16:07:47 +0100

    2025 d2 - refactor

--- src/aoc/2025/day02/solution.py
+++ src/aoc/2025/day02/solution.py
@@ -2,6 +2,17 @@ from math import ceil
 from pathlib import Path
 
 
+def validate(id_ranges: list[tuple[int, int]], max_len: int, max_groups: int) -> int:
+    invalid = set()
+    for k in range(2, max_groups + 1):
+        for n in range(10 ** ceil(max_len / k)):
+            rep = int(str(n) * k)
+            for l, r in id_ranges:
+                if l <= rep <= r:
+                    invalid.add(rep)
+    return sum(invalid)
+
+
 def main(input_path: Path):
     with input_path.open() as f:
         data = f.read()
@@ -13,46 +24,8 @@ def main(input_path: Path):
         max_len = max(max_len, len(up))
         id_ranges.append((int(low), int(up)))
 
-    invalid = set()
-    for k in range(2, max_len + 1):
-        for n in range(10 ** ceil(max_len / k)):
-            
-            rep = int(str(n) * k)
-            for l, r in id_ranges:
-                if l <= rep <= r:
-                    invalid.add(rep)
+    # ==== PART 1 ====
+    print(validate(id_ranges, max_len, max_groups=2))
 
-    print(sum(invalid))
-
-
-    # for l, r in id_ranges:
-    #     ll = len(l)
-    #     lr = len(r)
-
-    #     # both l, r have odd digits number
-    #     if ll % 2 and lr % 2:
-    #         continue
-
-    #     # l has odd digit number and r has even digit number
-    #     if ll % 2 and lr % 2 == 0:
-    #         start = 10 ** ll
-    #         end = int(r)
-        
-    #     # l has even digit number and r has odd digit number
-    #     elif ll % 2 == 0 and lr % 2:
-    #         start = int(l)
-    #         end = 10 ** (lr - 1) - 1
-
-    #     # both l, r have even digits number
-    #     else:
-    #         start, end = int(l), int(r)
-
-    #     for n in range(start, end + 1):
-
-
-
-
-        
-
-        
-
+    # ==== PART 2 ====
+    print(validate(id_ranges, max_len, max_groups=max_len))
