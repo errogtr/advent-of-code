9a904ab4b046341f0062c4ab62af2586f7c9cd96
enrico <enrico.dsf@gmail.com>
2025-12-03 04:17:47 +0100

    optmized 2025 d2 solution

--- src/aoc/2025/day02/solution.py
+++ src/aoc/2025/day02/solution.py
@@ -1,21 +1,33 @@
 from math import ceil
-from pathlib import Path
+
+from aoc.utils import read_data
 
 
-def validate(id_ranges: list[tuple[int, int]], max_len: int, max_groups: int) -> int:
+def validate(id_ranges: list[tuple[int, int]], max_groups: int) -> int:
     invalid = set()
-    for k in range(2, max_groups + 1):
-        for n in range(10 ** ceil(max_len / k)):
-            rep = int(str(n) * k)
-            for l, r in id_ranges:
-                if l <= rep <= r:
-                    invalid.add(rep)
+
+    for L, R in id_ranges:
+        for k in range(max_groups, 1, -1):
+            min_digits = max(1, len(str(L)) // k)
+            max_digits = len(str(R)) // k
+            for d in range(min_digits, max_digits + 1):
+                M = (10 ** (d * k) - 1) // (10 ** d - 1)
+
+                nd_low = 10 ** (d - 1)
+                nd_high = 10 ** d - 1
+
+                # Intersect with digit-range of n
+                n_low = max(ceil(L / M), nd_low)
+                n_high = min(R // M , nd_high)
+
+                invalid |= set(n*M for n in range(n_low, n_high + 1))
+
+
     return sum(invalid)
 
 
-def main(input_path: Path):
-    with input_path.open() as f:
-        data = f.read()
+def main():
+    data = read_data(__file__)
 
     max_len = 0  # max digits number
     id_ranges = list()  # list of int ranges [(lower bound, upper bound)]
@@ -25,7 +37,11 @@ def main(input_path: Path):
         id_ranges.append((int(low), int(up)))
 
     # ==== PART 1 ====
-    print(validate(id_ranges, max_len, max_groups=2))
+    print(validate(id_ranges, max_groups=2))
 
     # ==== PART 2 ====
-    print(validate(id_ranges, max_len, max_groups=max_len))
+    print(validate(id_ranges, max_groups=max_len))
+
+
+if __name__ == "__main__":
+    main()
--- src/aoc/utils.py
+++ src/aoc/utils.py
@@ -1,2 +1,12 @@
+from pathlib import Path
+
+
 def day_dir(day: int):
     return "day" + str(day).rjust(2, "0")
+
+
+def read_data(path: str, example: bool = False) -> str:
+    fname = "example" if example else "data"
+    with open(Path(path).parent / fname) as f:
+        data = f.read()
+    return data
