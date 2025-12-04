a6e899c4c40f729de5ec46fe8ee0abc6a143b335
enrico <enrico.dsf@gmail.com>
2025-12-04 03:08:20 +0100

    2025 d03 solution

--- src/aoc/2025/day03/solution.py
+++ src/aoc/2025/day03/solution.py
@@ -1,5 +1,29 @@
-from pathlib import Path
+def total_joltage(data, seq_len):
+    total = 0
+    for bank in data.split("\n"):
+        joltage = ""
+        start, end = 0, len(bank) - seq_len
+        while len(joltage) < seq_len:
+            window = bank[start:end+1]
+            m = max(window)
+            joltage += m
+            start += window.find(m) + 1
+            end +=1
+        total += int(joltage)
+    return total
 
 
-def main(input_path: Path):
-    pass
+
+def main():
+    with open("data") as f:
+        data = f.read()
+
+    # ==== PART 1 ====
+    print(total_joltage(data, 2))
+
+    # ==== PART 2 ====
+    print(total_joltage(data, 12))
+
+
+if __name__ == "__main__":
+    main()
