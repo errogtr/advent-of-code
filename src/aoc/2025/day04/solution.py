d9284055c702bf8cb435dba691cde3455de52575
enrico <enrico.dsf@gmail.com>
2025-12-04 14:18:04 +0100

    minor changes to 2025 d4

--- src/aoc/2025/day04/solution.py
+++ src/aoc/2025/day04/solution.py
@@ -1,35 +1,37 @@
-def accessible(z: complex, paper: set, nn: dict) -> bool:
-    return sum(w in paper for w in nn[z]) < 4
+NEIGH = [dx + 1j * dy for dx in (-1, 0, 1) for dy in (-1, 0, 1) if dx != 0 or dy != 0]
+
+
+def nn(z: complex):
+    for dz in NEIGH:
+        yield z + dz
+
+
+def accessible(z: complex, paper: set) -> bool:
+    return sum(w in paper for w in nn(z)) < 4
 
 
 def main():
     with open("src/aoc/2025/day04/data") as f:
         data = f.read()
 
-    paper, nn = set(), dict()
+    paper = set()
     for y, row in enumerate(data.splitlines()):
         for x, c in enumerate(row):
             if c == "@":
                 paper.add(x + 1j * y)
-            nn[x + 1j * y] = [
-                x + dx + 1j * (y + dy)
-                for dx in (-1, 0, 1)
-                for dy in (-1, 0, 1)
-                if dx != 0 or dy != 0
-            ]
-    
+
     # ==== PART 1 ====
-    print(sum(accessible(z, paper, nn) for z in paper))
+    print(sum(accessible(z, paper) for z in paper))
 
     # ==== PART 2 ====
     count = len(paper)
     while True:
-        removed = {z for z in paper if accessible(z, paper, nn)}
+        removed = {z for z in paper if accessible(z, paper)}
         if len(removed) == 0:
             break
         paper -= removed
     print(count - len(paper))
-        
+
 
 if __name__ == "__main__":
     main()
