9d24c8b7216bcae058f3ff1b7fb4f874b28640d9
enrico <enrico.dsf@gmail.com>
2025-12-05 11:44:58 +0100

    y2025 d5 solution

--- src/aoc/2025/day05/solution.py
+++ src/aoc/2025/day05/solution.py
@@ -1,5 +1,54 @@
-from pathlib import Path
+import click
+from aoc.utils import read_data, timer
 
 
-def main(input_path: Path):
-    pass
+@timer
+def part1(intervals, ids):
+    fresh = 0
+    for id in ids.splitlines():
+        for l, r in intervals:
+            if l <= int(id) <= r:
+                fresh += 1
+                break
+    return fresh
+
+
+@timer
+def part2(intervals):
+    merged_intervals = list()
+    for l, r in sorted(intervals):
+        if not merged_intervals:
+            merged_intervals.append((l, r))
+            continue
+        
+        prev_l, prev_r = merged_intervals[-1]
+        if l <= prev_r < r:
+            merged_intervals[-1] = (prev_l, r)
+        elif r <= prev_r:
+            continue
+        else:
+            merged_intervals.append((l, r))
+
+    return sum(r - l + 1 for l, r in merged_intervals)
+
+
+@click.command()
+@click.option("--example", is_flag=True)
+def main(example):
+    data = read_data(__file__, example)
+
+    id_ranges, ids = data.split("\n\n")
+    intervals = list()
+    for id_range in id_ranges.splitlines():
+        l, r = map(int, id_range.split("-"))
+        intervals.append((l, r))
+
+    # ==== PART 1 ====
+    print(part1(intervals, ids))
+
+    # ==== PART 2 ====
+    print(part2(intervals))
+
+
+if __name__ == "__main__":
+    main()
