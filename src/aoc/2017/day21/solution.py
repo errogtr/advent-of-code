2315800f96c179ddfd47f60e9ab2009f0522d07f
enrico <enrico.dsf@gmail.com>
2025-12-05 04:06:10 +0100

    y2017 d21 - rewritten

--- src/aoc/2017/day21/solution.py
+++ src/aoc/2017/day21/solution.py
@@ -1,11 +1,6 @@
-from copy import deepcopy
 import click
 
-from aoc.utils import read_data
-
-
-def print_img(img):
-    print("\n".join("".join(line) for line in img), "\n")
+from aoc.utils import read_data, timer
 
 
 def glue(img):
@@ -16,38 +11,57 @@ def separate(glued_img):
     return [list(line) for line in glued_img.split("/")]
 
 
-def rotate_90(glued_img):
+def rotate(glued_img):
     img = separate(glued_img)
     rotated = list(zip(*img[::-1]))
     return glue(rotated)
 
 
-def rotate_180(glued_img):
-    return rotate_90(rotate_90(glued_img))
-
-
-def rotate_270(glued_img):
-    return rotate_90(rotate_90(rotate_90(glued_img)))
-
-
 def flip_v(glued_img):
     img = separate(glued_img)
-    size = len(img)
-    flipped = [list() for _ in range(size)]
-    for i in range(size):
-        for j in range(size):
-            flipped[i].append(img[i][size - j - 1])
+    flipped = img[::-1]
     return glue(flipped)
 
 
 def flip_h(glued_img):
     img = separate(glued_img)
-    size = len(img)
-    flipped = [list() for _ in range(size)]
-    for i in range(size):
-        for j in range(size):
-            flipped[i].append(img[size - i - 1][j])
+    flipped = [row[::-1] for row in img]
     return glue(flipped)
+
+
+def enhance(img, img_size, s, rules):
+    enhanced = [list() for _ in range((img_size // s) * (s + 1))]
+    for i in range(0, img_size, s):
+        for j in range(0, img_size, s):
+            patch = [row[j : j + s] for row in img[i : i + s]]
+            enhanced_patch = separate(rules[glue(patch)])
+            for k in range((s + 1)):
+                for l in range((s + 1)):
+                    enhanced[k + (s + 1) * (i // s)].append(enhanced_patch[k][l])
+    return enhanced
+
+
+def iterate(rules, iterations):
+    image = [list(".#."), list("..#"), list("###")]
+
+    for _ in range(iterations):
+        size = len(image)
+        if size % 2 == 0:
+            image = enhance(image, size, 2, rules)
+        elif size % 3 == 0:
+            image = enhance(image, size, 3, rules)
+
+    return glue(image).count("#")
+
+
+@timer
+def part1(rules):
+    return iterate(rules, 5)
+
+
+@timer
+def part2(rules):
+    return iterate(rules, 18)
 
 
 @click.command()
@@ -58,71 +72,17 @@ def main(example):
     rules = dict()
     for line in data.splitlines():
         src, dst = line.split(" => ")
-        rules[src] = dst
-        rules[rotate_90(src)] = dst
-        rules[rotate_180(src)] = dst
-        rules[rotate_270(src)] = dst
-        rules[flip_v(src)] = dst
-        rules[rotate_90(flip_v(src))] = dst
-        rules[rotate_180(flip_v(src))] = dst
-        rules[rotate_270(flip_v(src))] = dst
-        rules[flip_h(src)] = dst
-        rules[rotate_90(flip_h(src))] = dst
-        rules[rotate_180(flip_h(src))] = dst
-        rules[rotate_270(flip_h(src))] = dst
-
-    image = [list(".#."), list("..#"), list("###")]
-    glued_img = glue(image)
-    for src in [
-        glued_img,
-        rotate_90(glued_img),
-        rotate_180(glued_img),
-        rotate_270(glued_img),
-        flip_v(glued_img),
-        flip_h(glued_img),
-    ]:
-        if src in rules:
-            dst = rules[src]
-            rules[rotate_90(src)] = dst
-            rules[rotate_180(src)] = dst
-            rules[rotate_270(src)] = dst
-            rules[flip_v(src)] = dst
+        for _ in range(4):
+            src = rotate(src)
+            rules[src] = dst
             rules[flip_h(src)] = dst
-            break
+            rules[flip_v(src)] = dst
 
-    iterations = 5
-    print_img(image)
-    for _ in range(iterations):
-        size = len(image)
+    # ==== PART 1 ====
+    print(part1(rules))
 
-        if size % 2 == 0:
-            enhanced = [list() for _ in range((size // 2) * 3)]
-            for i in range(0, size, 2):
-                for j in range(0, size, 2):
-                    patch = [
-                        image[i][j : j + 2],
-                        image[i + 1][j : j + 2],
-                    ]
-                    enhanced_patch = separate(rules[glue(patch)])
-                    for k in range(3):
-                        for l in range(3):
-                            enhanced[k + 3 * (i // 2)].append(enhanced_patch[k][l])
-        elif size % 3 == 0:
-            enhanced = [list() for _ in range((size // 3) * 4)]
-            for i in range(0, size, 3):
-                for j in range(0, size, 3):
-                    patch = [
-                        image[i][j : j + 3],
-                        image[i + 1][j : j + 3],
-                        image[i + 2][j : j + 3],
-                    ]
-                    enhanced_patch = separate(rules[glue(patch)])
-                    for k in range(4):
-                        for l in range(4):
-                            enhanced[k + 4 * (i // 3)].append(enhanced_patch[k][l])
-        image = enhanced
-
-    print(glue(image).count("#"))
+    # ==== PART 2 ====
+    print(part2(rules))
 
 
 if __name__ == "__main__":
