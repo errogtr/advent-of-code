995a8e8d1ff4097121bf3d7e7c6d530f9f534786
enrico <enrico.dsf@gmail.com>
2025-12-06 11:47:28 +0100

    y2025 d6 solution

--- src/aoc/2025/day06/solution.py
+++ src/aoc/2025/day06/solution.py
@@ -1,5 +1,54 @@
-from pathlib import Path
+from math import prod
+import click
+from aoc.utils import read_data, timer
 
 
-def main(input_path: Path):
-    pass
+OPS = {"+": sum, "*": prod}
+
+
+@timer
+def part1(data):
+    rows = [r.split() for r in data.splitlines()]
+
+    total = 0
+    for col in zip(*rows):
+        *nums, op = col
+        total += OPS[op](map(int, nums))
+    return total
+
+
+@timer
+def part2(data):
+    *num_rows, ops_row = data.splitlines()
+    nums_right_to_left = [r[::-1] for r in num_rows]
+
+    num_blocks = list()
+    current_block = list()
+    for col in zip(*nums_right_to_left):
+        digits = "".join(col).replace(" ", "")
+        if digits:
+            current_block.append(int(digits))
+        else:
+            num_blocks.append(current_block)
+            current_block = list()
+    num_blocks.append(current_block)
+
+    ops_right_to_left = [OPS[op] for op in ops_row.split()[::-1]]
+
+    return sum(op(block) for op, block in zip(ops_right_to_left, num_blocks))
+
+
+@click.command()
+@click.option("--example", is_flag=True)
+def main(example):
+    data = read_data(__file__, example)
+
+    # ==== PART 1 ====
+    print(part1(data))
+
+    # ==== PART 2 ====
+    print(part2(data))
+
+
+if __name__ == "__main__":
+    main()
