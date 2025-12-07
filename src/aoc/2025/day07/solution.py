bec3e7087a7008b695452bb95e3915c33b3b5fa7
enrico <enrico.dsf@gmail.com>
2025-12-07 16:26:52 +0100

    cleaned y2025 d7

--- src/aoc/2025/day07/solution.py
+++ src/aoc/2025/day07/solution.py
@@ -9,8 +9,7 @@ from aoc.utils import read_data, timer
 def part1(start, splitters):
     splits = 0
     beam = {start}
-    for row_splitters in splitters[1:]:
-        beam = {z + 1j for z in beam}
+    for row_splitters in splitters:
         for splitter in row_splitters & beam:
             beam.remove(splitter)
             beam |= {splitter - 1, splitter + 1}
@@ -21,8 +20,7 @@ def part1(start, splitters):
 @timer
 def part2(start, splitters):
     beam = Counter({start: 1})
-    for row_splitters in splitters[1:]:
-        beam = Counter({z + 1j: count for z, count in beam.items()})
+    for row_splitters in splitters:
         for splitter in row_splitters & set(beam):
             beam[splitter - 1] += beam[splitter]
             beam[splitter + 1] += beam[splitter]
@@ -38,16 +36,7 @@ def main(example):
     rows = data.splitlines()
     start = next(x for x, c in enumerate(rows[0]) if c == "S")
 
-    splitters = list()
-    for y, row in enumerate(rows[1:]):
-        row_splitters = set()
-        for x, c in enumerate(row):
-            z = x + 1j * y
-            if c == "S":
-                start = z
-            elif c == "^":
-                row_splitters.add(z)
-        splitters.append(row_splitters)
+    splitters = [{x for x, c in enumerate(row) if c == "^"} for row in rows[1:]]
 
     # ==== PART 1 ====
     print(part1(start, splitters))
