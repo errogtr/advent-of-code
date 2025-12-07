776d635e1be588b1a0f18199c6b4b235e09b915c
enrico <enrico.dsf@gmail.com>
2025-12-07 14:12:18 +0100

    minor refactor

--- src/aoc/2017/day25/solution.py
+++ src/aoc/2017/day25/solution.py
@@ -1,5 +1,31 @@
-from pathlib import Path
+import click
+
+from aoc.utils import read_data, timer
 
 
-def main(input_path: Path):
+@timer
+def part1():
     pass
+
+
+@timer
+def part2():
+    pass
+
+
+@click.command()
+@click.option("--example", is_flag=True)
+def main(example):
+    data = read_data(__file__, example)
+
+    instructions = data.splitlines()
+
+    # ==== PART 1 ====
+    print(part1())
+
+    # ==== PART 2 ====
+    print(part2())
+
+
+if __name__ == "__main__":
+    main()
