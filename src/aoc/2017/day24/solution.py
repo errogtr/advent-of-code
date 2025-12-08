220e6d98aff4a0e4685e9b4945772c5c2b5356ad
enrico <enrico.dsf@gmail.com>
2025-12-08 01:51:21 +0100

    ruff format

--- src/aoc/2017/day24/solution.py
+++ src/aoc/2017/day24/solution.py
@@ -12,12 +12,12 @@ def part1(connections):
 
         free_pin = pin_1 if used_pin == pin_2 else pin_2
         next_ports = connections[free_pin] - visited
-        
+
         if not next_ports:
             return strength
-        
+
         return max(strength + bridge(p, free_pin, visited | {p}) for p in next_ports)
-    
+
     return max(bridge(port, 0, {port}) for port in connections[0])
 
 
@@ -36,7 +36,6 @@ def main(example):
         pin_1, pin_2 = map(int, port.split("/"))
         connections[pin_1].add((pin_1, pin_2))
         connections[pin_2].add((pin_1, pin_2))
-
 
     # ==== PART 1 ====
     print(part1(connections))
