611e7c345b7f27dd0a3cdb43117ca8481e707842
enrico <enrico.dsf@gmail.com>
2026-01-10 21:17:56 +0100

    y2018 d3-d25 templates

--- src/aoc/2018/day21/solution.py
+++ src/aoc/2018/day21/solution.py
@@ -1,5 +1,29 @@
-from pathlib import Path
+import click
+from aoc.utils import read_data, timer
 
 
-def main(input_path: Path):
+@timer
+def part1():
     pass
+
+
+@timer
+def part2():
+    pass
+
+
+@click.command()
+@click.option("--example", is_flag=True)
+def main(example: bool):
+    data = read_data(__file__, example)
+
+    # ==== PART 1 ====
+    print(part1())
+
+    # ==== PART 2 ====
+    print(part2())
+
+
+if __name__ == "__main__":
+    main()
+
