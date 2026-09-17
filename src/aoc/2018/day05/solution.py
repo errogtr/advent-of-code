0f69eb6715553e24a47018d46443aba4bf34f025
enrico <enrico.dsf@gmail.com>
2026-09-18 01:44:11 +0200

    y2018d05

--- src/aoc/2018/day05/solution.py
+++ src/aoc/2018/day05/solution.py
@@ -1,15 +1,41 @@
+import string
 import click
 from aoc.utils import read_data, timer
 
 
-@timer
-def part1():
-    pass
+# fmt: off
+reactions = {
+    'aA', 'bB', 'cC', 'dD', 'eE', 'fF', 'gG', 'hH', 'iI', 'jJ', 'kK', 'lL', 'mM', 
+    'nN', 'oO', 'pP', 'qQ', 'rR', 'sS', 'tT', 'uU', 'vV', 'wW', 'xX', 'yY', 'zZ',
+    'Aa', 'Bb', 'Cc', 'Dd', 'Ee', 'Ff', 'Gg', 'Hh', 'Ii', 'Jj', 'Kk', 'Ll', 'Mm', 
+    'Nn', 'Oo', 'Pp', 'Qq', 'Rr', 'Ss', 'Tt', 'Uu', 'Vv', 'Ww', 'Xx', 'Yy', 'Zz',
+    }
+# fmt: on
+
+
+def react(polymer):
+    i = 0
+    while i < len(polymer):
+        if polymer[i : i + 2] in reactions:
+            polymer = polymer[:i] + polymer[i + 2 :]
+            i -= 1
+        else:
+            i += 1
+    return polymer
 
 
 @timer
-def part2():
-    pass
+def part1(polymer):
+    return len(react(polymer))
+
+
+@timer
+def part2(polymer):
+    shortest = len(polymer)
+    for unit in string.ascii_lowercase:
+        modified = polymer.replace(unit, "").replace(unit.upper(), "")
+        shortest = min(shortest, len(react(modified)))
+    return shortest
 
 
 @click.command()
@@ -18,12 +44,11 @@ def main(example: bool):
     data = read_data(__file__, example)
 
     # ==== PART 1 ====
-    print(part1())
+    print(part1(data))
 
     # ==== PART 2 ====
-    print(part2())
+    print(part2(data))
 
 
 if __name__ == "__main__":
     main()
-
