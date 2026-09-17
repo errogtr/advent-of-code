0a142f0d72b19679f49119a6dae747766be58d0b
enrico <enrico.dsf@gmail.com>
2026-09-17 23:39:19 +0200

    y2018d03

--- src/aoc/2018/day03/solution.py
+++ src/aoc/2018/day03/solution.py
@@ -1,10 +1,33 @@
+from collections import Counter
+from itertools import product
+import re
 import click
 from aoc.utils import read_data, timer
 
 
 @timer
-def part1():
-    pass
+def part12(data):
+    patches = list()
+    for claim in data.splitlines():
+        m = re.match(r"^#(\d+) @ (\d+),(\d+): (\d+)x(\d+)$", claim)
+        c, x, y, w, h = map(int, m.groups())
+        patches.append((c, x, y, w, h))
+
+    fabric = Counter()
+    for pc, px, py, pw, ph in patches:
+        for x, y in product(range(px, px + pw), range(py, py + ph)):
+            fabric[(x, y)] += 1
+
+    intact = next(
+        pc
+        for pc, px, py, pw, ph in patches
+        if all(
+            fabric[(x, y)] == 1
+            for x, y in product(range(px, px + pw), range(py, py + ph))
+        )
+    )
+
+    return sum(c > 1 for c in fabric.values()), intact
 
 
 @timer
@@ -17,13 +40,9 @@ def part2():
 def main(example: bool):
     data = read_data(__file__, example)
 
-    # ==== PART 1 ====
-    print(part1())
-
-    # ==== PART 2 ====
-    print(part2())
+    # ==== PART 1 & 2====
+    print(part12(data))
 
 
 if __name__ == "__main__":
     main()
-
