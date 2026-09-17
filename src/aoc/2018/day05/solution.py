39ff0012e70631edee88ac0250c3f90affc7099a
enrico <enrico.dsf@gmail.com>
2026-09-18 01:55:42 +0200

    optimized more y2018d05

--- src/aoc/2018/day05/solution.py
+++ src/aoc/2018/day05/solution.py
@@ -3,25 +3,14 @@ import click
 from aoc.utils import read_data, timer
 
 
-# fmt: off
-reactions = {
-    'aA', 'bB', 'cC', 'dD', 'eE', 'fF', 'gG', 'hH', 'iI', 'jJ', 'kK', 'lL', 'mM', 
-    'nN', 'oO', 'pP', 'qQ', 'rR', 'sS', 'tT', 'uU', 'vV', 'wW', 'xX', 'yY', 'zZ',
-    'Aa', 'Bb', 'Cc', 'Dd', 'Ee', 'Ff', 'Gg', 'Hh', 'Ii', 'Jj', 'Kk', 'Ll', 'Mm', 
-    'Nn', 'Oo', 'Pp', 'Qq', 'Rr', 'Ss', 'Tt', 'Uu', 'Vv', 'Ww', 'Xx', 'Yy', 'Zz',
-    }
-# fmt: on
-
-
 def react(polymer):
-    i = 0
-    while i < len(polymer):
-        if polymer[i : i + 2] in reactions:
-            polymer = polymer[:i] + polymer[i + 2 :]
-            i -= 1
+    stack = []
+    for unit in polymer:
+        if stack and stack[-1] != unit and stack[-1].lower() == unit.lower():
+            stack.pop()
         else:
-            i += 1
-    return polymer
+            stack.append(unit)
+    return "".join(stack)
 
 
 @timer
