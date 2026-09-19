18444d20c270651022c48a95e546385653010062
enrico <enrico.dsf@gmail.com>
2026-09-19 14:26:51 +0200

    y2018d07p1

--- src/aoc/2018/day07/solution.py
+++ src/aoc/2018/day07/solution.py
@@ -1,10 +1,45 @@
+from collections import defaultdict
+import re
+
 import click
+
 from aoc.utils import read_data, timer
 
 
+instr_pattern = re.compile(
+    r"Step ([A-Z]) must be finished before step ([A-Z]) can begin."
+)
+
+
+def traverse(node, graph, executed):
+    if node not in graph:
+        node
+
+    steps = sorted(graph[node])
+    order = node
+    for step in steps:
+        executed[step][node] = True
+        if all(executed[step].values()):
+            order += traverse(step, graph, executed)
+    return order
+
+
 @timer
-def part1():
-    pass
+def part1(data):
+    graph = defaultdict(list)
+    executed = defaultdict(dict)
+    nodes = set()
+    for instruction in data.splitlines():
+        step, next_step = instr_pattern.search(instruction).groups()
+        graph[step].append(next_step)
+        executed[next_step][step] = False
+        nodes |= {step, next_step}
+
+    order = ""
+    stack = sorted(list(nodes - set(executed)))
+    for node in stack:
+        order += traverse(node, graph, executed)
+    return order
 
 
 @timer
@@ -18,7 +53,7 @@ def main(example: bool):
     data = read_data(__file__, example)
 
     # ==== PART 1 ====
-    print(part1())
+    print(part1(data))
 
     # ==== PART 2 ====
     print(part2())
@@ -26,4 +61,3 @@ def main(example: bool):
 
 if __name__ == "__main__":
     main()
-
